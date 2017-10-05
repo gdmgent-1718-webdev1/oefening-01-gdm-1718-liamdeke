ad2aefad73c3543b250da6a184f81f4bfaa145b3
gdm-1718-liamdeke <liamdeke@student.arteveldehs.be>
2017-10-05 10:27:21 +0200

    update

--- index.php
+++ index.php
@@ -21,37 +21,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><?= $name ?></h1>
-
-    <p><?= $string1 ." ". $string2 ?></p>
-
-    <div class= "Wiskunde">
-        <?= $y ." + ". $x ." = ". ($x + $y) ?> </br>
-        <?= $y ." - ". $x ." = ". ($y - $x) ?> </br>
-        <?= $y ." * ". $x ." = ". ($x * $y) ?> </br>
-        <?= $y ." / ". $x ." = ". ($y / $x) ?> </br>
-        <?= $y ." % ". $x ." = ". ($y % $x) ?>
-    </div>
-
-    <div class = "StringTypes">
-        <p>
-        De Waarde is <?= gettype($watIs) ?>
-        </p>
-    </div>
-
-    <div>
-        <?php
-        if( $age < 16) {
-            echo $age ." Is te jong voor deze film.";
-        } 
-        elseif( $age >= 16 && $age <= 18) {
-            echo $age ." Je hebt ouderlijk toezicht nodig.";
-        }
-        else {
-            echo "Je bent reeds ".$age.". Geniet van de film";
-        }
-        
-        ?>
-    </div>
+    <h1>Welkom <?= $name ?>!</h1>
 </body>
 </html>
